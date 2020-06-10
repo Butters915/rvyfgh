299374f7b65262400e710c5348ae54b104ac5b67
Butters915 <butters9156@gmail.com>
2020-06-09 21:33:57 -0600

    Broke almost everything lmao

--- Assets/Scenes/player.gd
+++ Assets/Scenes/player.gd
@@ -3,9 +3,10 @@ extends KinematicBody2D
 var motion = Vector2();
 var gravity = Vector2();
 
+const UP = Vector2(0, -1)
+
 func _physics_process(delta):
 	
-	print(motion.y);
 	
 	var kLeft = Input.is_action_pressed("ui_left");
 	var kRight = Input.is_action_pressed("ui_right");
@@ -15,25 +16,19 @@ func _physics_process(delta):
 	
 	motion.x = move*500;
 	
-	if(kJump == true):
-		gravity.y -= 5000;
-	
 	motion.y = gravity.y;
 	gravity.y +=10;
 	
-	
-	
-	
-	if(sign(int(kJump)) > 0):
-		gravity.y=0;
-		pass
-	
-	print(int(motion.x));
-	
-	
-	
 	motion.x += move;
-	move_and_slide(motion);
+	move_and_slide(motion, UP);
 	motion.x=0;
+	
+	if(is_on_floor() == true && int(kJump) > 0):
+		print("Jump")
+		for n in 1:
+			gravity.y -= -500 * delta;
+	
+	if(is_on_floor() == true):
+		gravity.y == 0;
 	
 	pass
