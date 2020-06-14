6374bfe7c6a60135d6d481f7d135150429109b65
Butters915 <butters9156@gmail.com>
2020-06-13 23:56:31 -0600

    idk lol

--- Assets/Scenes/player.gd
+++ Assets/Scenes/player.gd
@@ -1,34 +1,43 @@
 extends KinematicBody2D
 
-var motion = Vector2();
-var gravity = Vector2();
-
-const UP = Vector2(0, -1)
+var grav = 0.5; 
+var hsp = 0; 
+var vsp = 0;
+var mvsp = 50;
+var mxsp = 1;
+var jumpSpeed = 10; 
+var moveSpeed = 5;
+var jmp = false;
 
 func _physics_process(delta):
-	
-	
 	var kLeft = Input.is_action_pressed("ui_left");
-	var kRight = Input.is_action_pressed("ui_right");
+	var kRight = Input.is_action_pressed("ui_right"); 
 	var kJump = Input.is_action_pressed("ui_up");
 	
-	var move = -int(kLeft) + int(kRight);
+	var move = kLeft + kRight; 
+	var hsp = move * moveSpeed; 
 	
-	motion.x = move*500;
+	if (vsp < mvsp):
+		vsp += grav; 
 	
-	motion.y = gravity.y;
-	gravity.y +=10;
+	if (kJump && place_meeting(x, y+35, obj_wall)):
+		jmp = true; 
+		
+	if (place_meeting(x, y + 1, obj_wall)):
+		if(jmp == true):
+			vsp = 1 * -jumpSpeed; 
+		jmp = false
 	
-	motion.x += move;
-	move_and_slide(motion, UP);
-	motion.x=0;
+	if (place_meeting(x + hsp, y, obj_wall)):
+		while (!place_meeting(x + sign(hsp), y, obj_wall)):
+			x += (dt * sign(hsp)); 
+			x = lerp(x, hsp, 0.1);
+		hsp = 0; 
+	x += hsp;
 	
-	if(is_on_floor() == true && int(kJump) > 0):
-		print("Jump")
-		for n in 1:
-			gravity.y -= -500 * delta;
-	
-	if(is_on_floor() == true):
-		gravity.y == 0;
-	
+	if (place_meeting(x, y + vsp, obj_wall)):
+		while (!place_meeting(x, y + sign(vsp), obj_wall)):
+			y += sign(vsp); 
+			vsp = 0; 
+	y += vsp;
 	pass
